7d37fb87d23fb3148cb267c96376473876db787c
Chengzhi <yu000209@algonquinlive.com>
2020-06-22 01:10:09 -0400

    Finished lab4

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidlabs;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -18,26 +19,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChatRoomActivity extends AppCompatActivity {
-    private List<MessageHandler> listMessages = new ArrayList<>();
-    private ListView listView;
-    private EditText textMessage;
+    private ArrayList<MessageHandler> listMessages = new ArrayList<>();
     private MyListAdapter  myAdapter = new MyListAdapter();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
 
-        listView = findViewById(R.id.list_view);
+        ListView listView = findViewById(R.id.list_view);
         Button btSend = findViewById(R.id.send);
         Button btReceive = findViewById(R.id.receive);
-        textMessage = findViewById(R.id.message_to_send);
-
+        EditText textMessage = findViewById(R.id.message_to_send);
+        listView.setAdapter(myAdapter);
         btSend.setOnClickListener(e -> {
 
             String message = textMessage.getText().toString();
             MessageHandler messageSend = new MessageHandler(message, true);
             listMessages.add(messageSend);
-            listView.setAdapter(myAdapter);
             myAdapter.notifyDataSetChanged();
             textMessage.setText("");
         });
@@ -46,15 +45,27 @@ public class ChatRoomActivity extends AppCompatActivity {
             String message = textMessage.getText().toString();
             MessageHandler messageReceive = new MessageHandler(message, false);
             listMessages.add(messageReceive);
-            listView.setAdapter(myAdapter);
             myAdapter.notifyDataSetChanged();
             textMessage.setText("");
         });
 
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("Do you want to delete this?")
+                    .setMessage("The selected row is: " + (position + 1) + "\n" + "The database id is:" + id
+)
+                    .setPositiveButton("Yes", (click, arg) -> {
+                        listMessages.remove(position);
+                        myAdapter.notifyDataSetChanged();
+                    })
+                    .setNegativeButton("No", (click, arg) -> { })
+                    .create().show();
+
+            return true;
+        });
     }
 
    private class MyListAdapter extends BaseAdapter {
-
        @Override
        public int getCount() {
            return listMessages.size();
@@ -73,9 +84,10 @@ public class ChatRoomActivity extends AppCompatActivity {
        @Override
        public View getView(int position, View convertView, ViewGroup parent) {
            View view = convertView;
+           MessageHandler messageHandler = (MessageHandler)getItem(position);
            LayoutInflater inflater = getLayoutInflater();
-           if (view == null){
-               if(listMessages.get(position).isSend()){
+          //if (view == null){
+               if(messageHandler.isSend()){
                    view = inflater.inflate(R.layout.send_message,parent,false);
                }
                else{
@@ -84,8 +96,8 @@ public class ChatRoomActivity extends AppCompatActivity {
                TextView message = (TextView) view.findViewById(R.id.textMessage);
                message.setTextColor(Color.BLACK);
                message.setTextSize(46);
-               message.setText(listMessages.get(position).getMessage());
-           }
+               message.setText(messageHandler.getMessage());
+         // }
            return view;
        }
    }
